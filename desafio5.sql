8ea0f842ce9b2e185c00a9e3c5c67b49d7370af3
PatriciaDivi <patriciavianad@gmail.com>
2022-06-06 21:51:37 -0300

    5 Requisito | finalizado | :rocket:

---
+++ desafio5.sql
@@ -0,0 +1,13 @@
+SELECT
+    song.`name` AS cancao,
+    COUNT(user_history.song_id) AS reproducoes
+FROM
+    song
+    JOIN user_history ON song.id = user_history.song_id
+GROUP BY
+    cancao
+ORDER BY
+    reproducoes DESC,
+    cancao
+LIMIT
+    2;
